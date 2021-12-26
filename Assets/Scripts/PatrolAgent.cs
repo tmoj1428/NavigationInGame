d9b5978af1c23a6f2a24b3e2becd0861b27667ae
mohammad javad taheri <tmoj.1428@gmail.com>
2021-12-26 21:36:59 +0330

    Add project

---
+++ Assets/Scripts/PatrolAgent.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolAgent : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] points;
+
+    private int destinationPoint = 0;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false; // agent does not slow down when it get to a point
+        GoToNextPoint();
+    }
+
+    private void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.2) // is agent in processing of the path or not
+        {
+            GoToNextPoint();
+        }
+    }
+
+    void GoToNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            Debug.LogError("Set Dest please");
+            enabled = false;
+            return;
+        }
+
+        agent.destination = points[destinationPoint].position;
+        destinationPoint = (destinationPoint + 1) % points.Length;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var playerAgent = GetComponent<NavMeshAgent>();
+        playerAgent.destination = goal.position;
+    }
+}
